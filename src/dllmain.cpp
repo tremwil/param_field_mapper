19965dc2ed9dbfe02885e688517182e6fe7c0a30
William Tremblay <tremwil@gmail.com>
2023-09-04 12:41:48 -0400

    Fix tab/space inconsistency

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -19,12 +19,12 @@ using namespace spdlog;
 using namespace std::chrono_literals;
 
 typedef NTSTATUS (NTAPI *pNtGetNextThread)(
-         HANDLE ProcessHandle,
-         HANDLE ThreadHandle,
-         ACCESS_MASK DesiredAccess,
-         ULONG HandleAttributes,
-         ULONG Flags,
-         PHANDLE NewThreadHandle
+        HANDLE ProcessHandle,
+        HANDLE ThreadHandle,
+        ACCESS_MASK DesiredAccess,
+        ULONG HandleAttributes,
+        ULONG Flags,
+        PHANDLE NewThreadHandle
 );
 
 typedef NTSTATUS (NTAPI *pNtQueryInformationThread)(
@@ -85,77 +85,77 @@ BOOL hijack_suspended_main_thread(void* hook, uintptr_t (**original_entry_point)
 }
 
 void create_console() {
-	if (!GetConsoleWindow() && !AllocConsole()) {
-		Panic("AllocConsole failed (error 0x{:x})", GetLastError());
-	}
+    if (!GetConsoleWindow() && !AllocConsole()) {
+        Panic("AllocConsole failed (error 0x{:x})", GetLastError());
+    }
 
-	// std::cout, std::clog, std::cerr, std::cin
-	FILE* fDummy;
-	freopen_s(&fDummy, "CONOUT$", "w", stdout);
-	freopen_s(&fDummy, "CONOUT$", "w", stderr);
-	freopen_s(&fDummy, "CONIN$", "r", stdin);
-	std::cout.clear();
-	std::clog.clear();
-	std::cerr.clear();
-	std::cin.clear();
+    // std::cout, std::clog, std::cerr, std::cin
+    FILE* fDummy;
+    freopen_s(&fDummy, "CONOUT$", "w", stdout);
+    freopen_s(&fDummy, "CONOUT$", "w", stderr);
+    freopen_s(&fDummy, "CONIN$", "r", stdin);
+    std::cout.clear();
+    std::clog.clear();
+    std::cerr.clear();
+    std::cin.clear();
 
-	// std::wcout, std::wclog, std::wcerr, std::wcin
-	HANDLE hConOut = CreateFile(TEXT("CONOUT$"), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	HANDLE hConIn = CreateFile(TEXT("CONIN$"), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	SetStdHandle(STD_OUTPUT_HANDLE, hConOut);
-	SetStdHandle(STD_ERROR_HANDLE, hConOut);
-	SetStdHandle(STD_INPUT_HANDLE, hConIn);
-	std::wcout.clear();
-	std::wclog.clear();
-	std::wcerr.clear();
-	std::wcin.clear();
+    // std::wcout, std::wclog, std::wcerr, std::wcin
+    HANDLE hConOut = CreateFile(TEXT("CONOUT$"), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    HANDLE hConIn = CreateFile(TEXT("CONIN$"), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    SetStdHandle(STD_OUTPUT_HANDLE, hConOut);
+    SetStdHandle(STD_ERROR_HANDLE, hConOut);
+    SetStdHandle(STD_INPUT_HANDLE, hConIn);
+    std::wcout.clear();
+    std::wclog.clear();
+    std::wcerr.clear();
+    std::wcin.clear();
 }
 
 void bootstrap(bool is_entry_hook) {
-	create_console();
-	
-  	std::locale::global(std::locale("en_us.UTF-8"));
-	set_pattern("[%T.%e] %^[%l]%$ [%s] %v");
-	set_level(level::level_enum::debug);
-	flush_on(level::level_enum::err);
+    create_console();
+    
+    std::locale::global(std::locale("en_us.UTF-8"));
+    set_pattern("[%T.%e] %^[%l]%$ [%s] %v");
+    set_level(level::level_enum::debug);
+    flush_on(level::level_enum::err);
 
-	if (!is_entry_hook) {
-		SPDLOG_WARN("DLL was injected without launcher. Arxan has already run; code analysis coverage will not be as good!");
-	}
-	else {
-		// Since we're entry hooking, Arxan hasn't set the game's memory to RWE yet. Do it now
-		auto main_module = mem::module::main();
-		DWORD old_protect;
-		if (!VirtualProtect(main_module.start.as<void*>(), main_module.size, PAGE_EXECUTE_READWRITE, &old_protect)) {
-			Panic("Failed to change memory protection of game code (error {:08X})", GetLastError());
-		}
-	}
+    if (!is_entry_hook) {
+        SPDLOG_WARN("DLL was injected without launcher. Arxan has already run; code analysis coverage will not be as good!");
+    }
+    else {
+        // Since we're entry hooking, Arxan hasn't set the game's memory to RWE yet. Do it now
+        auto main_module = mem::module::main();
+        DWORD old_protect;
+        if (!VirtualProtect(main_module.start.as<void*>(), main_module.size, PAGE_EXECUTE_READWRITE, &old_protect)) {
+            Panic("Failed to change memory protection of game code (error {:08X})", GetLastError());
+        }
+    }
 
     arxan_disabler::disable_code_restoration();
-	ParamFieldMapper::get().init();
+    ParamFieldMapper::get().init();
 }
 
 static uintptr_t(*ORIGINAL_ENTRY_POINT)();
 uintptr_t __stdcall hooked_entry_point() {
-	bootstrap(true);
-	return ORIGINAL_ENTRY_POINT();
+    bootstrap(true);
+    return ORIGINAL_ENTRY_POINT();
 }
 
 DWORD __stdcall thread_entry_point(LPVOID lParam) {
-	bootstrap(false);
-	return 0;
+    bootstrap(false);
+    return 0;
 }
 
 BOOL APIENTRY DllMain(HMODULE module, DWORD call_reason, LPVOID reserved) {
-	DisableThreadLibraryCalls(module);
-	if (call_reason != DLL_PROCESS_ATTACH) { 
-		return TRUE;
-	}
-	if (hijack_suspended_main_thread((void*)hooked_entry_point, &ORIGINAL_ENTRY_POINT)) {
-		return TRUE;
-	}
-	else {
-		auto handle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread_entry_point, module, 0, NULL);
-		return handle != INVALID_HANDLE_VALUE;
-	}
+    DisableThreadLibraryCalls(module);
+    if (call_reason != DLL_PROCESS_ATTACH) { 
+        return TRUE;
+    }
+    if (hijack_suspended_main_thread((void*)hooked_entry_point, &ORIGINAL_ENTRY_POINT)) {
+        return TRUE;
+    }
+    else {
+        auto handle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread_entry_point, module, 0, NULL);
+        return handle != INVALID_HANDLE_VALUE;
+    }
 };
--- src/hooks/instr_utils.cpp
+++ src/hooks/instr_utils.cpp
@@ -8,110 +8,110 @@
 
 namespace pfm::instr_utils
 {
-	bool gen_lea(LiteMemStream* out_stream, hde64s* instr)
-	{
-		if (!(instr->flags & F_MODRM))
-			return false;
+    bool gen_lea(LiteMemStream* out_stream, hde64s* instr)
+    {
+        if (!(instr->flags & F_MODRM))
+            return false;
 
-		// Set REX.W (64bit dest) and clear REX.R (makes rax -> r8)
-		//printf("REX: %02X\n", instr->rex);
-		out_stream->write<uint8_t>((instr->rex & 0b01001011) | 0b01001000);
-		out_stream->write<uint8_t>(0x8d);
+        // Set REX.W (64bit dest) and clear REX.R (makes rax -> r8)
+        //printf("REX: %02X\n", instr->rex);
+        out_stream->write<uint8_t>((instr->rex & 0b01001011) | 0b01001000);
+        out_stream->write<uint8_t>(0x8d);
 
-		// Clear modrm.reg to 0 for rax
-		out_stream->write<uint8_t>(instr->modrm & 0b11000111);
-		if (instr->flags & F_SIB) out_stream->write<uint8_t>(instr->sib);
+        // Clear modrm.reg to 0 for rax
+        out_stream->write<uint8_t>(instr->modrm & 0b11000111);
+        if (instr->flags & F_SIB) out_stream->write<uint8_t>(instr->sib);
 
-		if (instr->flags & F_DISP8)
-			out_stream->write<uint8_t>(instr->disp.disp8);
-		else if (instr->flags & F_DISP32)
-			out_stream->write<uint32_t>(instr->disp.disp32);
+        if (instr->flags & F_DISP8)
+            out_stream->write<uint8_t>(instr->disp.disp8);
+        else if (instr->flags & F_DISP32)
+            out_stream->write<uint32_t>(instr->disp.disp32);
 
-		return true;
-	}
+        return true;
+    }
 
-	int32_t get_disp(uint8_t* instr) 
-	{
-		hde64s disasm;
-		hde64_disasm(instr, &disasm);
+    int32_t get_disp(uint8_t* instr) 
+    {
+        hde64s disasm;
+        hde64_disasm(instr, &disasm);
 
-		if (!(disasm.flags & F_MODRM))
-			return 0;
+        if (!(disasm.flags & F_MODRM))
+            return 0;
 
-		if (disasm.flags & F_DISP8) return (int8_t)disasm.disp.disp8;
-		if (disasm.flags & F_DISP16) return (int16_t)disasm.disp.disp16;
-		if (disasm.flags & F_DISP32) return (int16_t)disasm.disp.disp32;
-		else return 0;
-	}
+        if (disasm.flags & F_DISP8) return (int8_t)disasm.disp.disp8;
+        if (disasm.flags & F_DISP16) return (int16_t)disasm.disp.disp16;
+        if (disasm.flags & F_DISP32) return (int16_t)disasm.disp.disp32;
+        else return 0;
+    }
 
-	size_t gen_new_disp(uint8_t* instr_out, uint8_t* instr, int32_t new_disp)
-	{
-		hde64s disasm;
-		hde64_disasm(instr, &disasm);
+    size_t gen_new_disp(uint8_t* instr_out, uint8_t* instr, int32_t new_disp)
+    {
+        hde64s disasm;
+        hde64_disasm(instr, &disasm);
 
-		if (!(disasm.flags & F_MODRM))
-			return 0;
+        if (!(disasm.flags & F_MODRM))
+            return 0;
 
-		int32_t imm_size = 0;
-		if (disasm.flags & F_IMM8) imm_size = 1;
-		else if (disasm.flags & F_IMM16) imm_size = 2;
-		else if (disasm.flags & F_IMM32) imm_size = 4;
-		else if (disasm.flags & F_IMM64) return 0; // Not supported
+        int32_t imm_size = 0;
+        if (disasm.flags & F_IMM8) imm_size = 1;
+        else if (disasm.flags & F_IMM16) imm_size = 2;
+        else if (disasm.flags & F_IMM32) imm_size = 4;
+        else if (disasm.flags & F_IMM64) return 0; // Not supported
 
-		int32_t imm_offset = disasm.len - imm_size;
-		int32_t disp_offset = imm_offset;
+        int32_t imm_offset = disasm.len - imm_size;
+        int32_t disp_offset = imm_offset;
 
-		if (disasm.modrm_mod == 1) disp_offset -= 1;
-		if (disasm.modrm_mod == 2) disp_offset -= 4;
+        if (disasm.modrm_mod == 1) disp_offset -= 1;
+        if (disasm.modrm_mod == 2) disp_offset -= 4;
 
-		int32_t new_instr_size = disp_offset + imm_size + 4;
-		int32_t modrm_byte = disp_offset - ((disasm.flags & F_SIB) ? 2 : 1);
+        int32_t new_instr_size = disp_offset + imm_size + 4;
+        int32_t modrm_byte = disp_offset - ((disasm.flags & F_SIB) ? 2 : 1);
 
-		memcpy(instr_out, instr, disasm.len);
-		memcpy(instr_out + disp_offset + 4, instr + imm_offset, imm_size);
+        memcpy(instr_out, instr, disasm.len);
+        memcpy(instr_out + disp_offset + 4, instr + imm_offset, imm_size);
 
-		instr_out[modrm_byte] = (disasm.modrm & 0x3F) | 0x80;
-		*(uint32_t*)(instr_out + disp_offset) = new_disp;
+        instr_out[modrm_byte] = (disasm.modrm & 0x3F) | 0x80;
+        *(uint32_t*)(instr_out + disp_offset) = new_disp;
 
-		return new_instr_size;
-	}
+        return new_instr_size;
+    }
 
-	bool jmp_targets_heuristic(const char* mod_name, std::vector<intptr_t>& targets)
-	{
-		HMODULE h_mod = GetModuleHandleA(mod_name);
-		if (h_mod == NULL) return false;
+    bool jmp_targets_heuristic(const char* mod_name, std::vector<intptr_t>& targets)
+    {
+        HMODULE h_mod = GetModuleHandleA(mod_name);
+        if (h_mod == NULL) return false;
 
-		IMAGE_DOS_HEADER* dos = (IMAGE_DOS_HEADER*)h_mod;
-		IMAGE_NT_HEADERS* nt = (IMAGE_NT_HEADERS*)((uint8_t*)h_mod + dos->e_lfanew);
+        IMAGE_DOS_HEADER* dos = (IMAGE_DOS_HEADER*)h_mod;
+        IMAGE_NT_HEADERS* nt = (IMAGE_NT_HEADERS*)((uint8_t*)h_mod + dos->e_lfanew);
 
-		IMAGE_SECTION_HEADER* sections = IMAGE_FIRST_SECTION(nt);
+        IMAGE_SECTION_HEADER* sections = IMAGE_FIRST_SECTION(nt);
 
-		for (int i = 0; i < nt->FileHeader.NumberOfSections; i++)
-		{
-			if (!(sections[i].Characteristics & IMAGE_SCN_MEM_EXECUTE) || strcmp((char*)sections[i].Name, ".text")) continue;
+        for (int i = 0; i < nt->FileHeader.NumberOfSections; i++)
+        {
+            if (!(sections[i].Characteristics & IMAGE_SCN_MEM_EXECUTE) || strcmp((char*)sections[i].Name, ".text")) continue;
 
-			uint8_t* base = (uint8_t*)h_mod + sections[i].VirtualAddress;
-			uint8_t* end = base + sections[i].Misc.VirtualSize;
+            uint8_t* base = (uint8_t*)h_mod + sections[i].VirtualAddress;
+            uint8_t* end = base + sections[i].Misc.VirtualSize;
 
-			hde64s instr, jmp_check;
-			std::set<intptr_t> targets_set;
-			for (uint8_t* cptr = base; cptr < end; cptr++)
-			{
-				uint8_t* cins = cptr;
-				if (hde64_disasm(cins, &instr) & F_ERROR) continue;
-				if (!(instr.flags & F_RELATIVE) || !(instr.flags & (F_IMM8 | F_IMM32))) continue;
+            hde64s instr, jmp_check;
+            std::set<intptr_t> targets_set;
+            for (uint8_t* cptr = base; cptr < end; cptr++)
+            {
+                uint8_t* cins = cptr;
+                if (hde64_disasm(cins, &instr) & F_ERROR) continue;
+                if (!(instr.flags & F_RELATIVE) || !(instr.flags & (F_IMM8 | F_IMM32))) continue;
 
-				// Check if instruction is "legit" by requiring that it also points to a sequence of valid instruction
-				uint8_t* jmp_target = cins + instr.len + ((instr.flags & F_IMM8) ? (int8_t)instr.imm.imm8 : (int32_t)instr.imm.imm32);
-				if (jmp_target < base || jmp_target >= end) continue;
-				if (hde64_disasm(jmp_target, &jmp_check) & F_ERROR) continue;
+                // Check if instruction is "legit" by requiring that it also points to a sequence of valid instruction
+                uint8_t* jmp_target = cins + instr.len + ((instr.flags & F_IMM8) ? (int8_t)instr.imm.imm8 : (int32_t)instr.imm.imm32);
+                if (jmp_target < base || jmp_target >= end) continue;
+                if (hde64_disasm(jmp_target, &jmp_check) & F_ERROR) continue;
 
-				targets_set.insert((intptr_t)jmp_target);
-			}
-			targets.assign(targets_set.begin(), targets_set.end());
-			return true;
-		}
-		return false;
-	}
+                targets_set.insert((intptr_t)jmp_target);
+            }
+            targets.assign(targets_set.begin(), targets_set.end());
+            return true;
+        }
+        return false;
+    }
 }
 
