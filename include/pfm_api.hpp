00e866d481dfdd2e0683e7cce56674c4d28d494a
William Tremblay <tremwil@gmail.com>
2023-09-06 01:36:51 -0400

    Removed unsured details namespace

--- include/pfm_api.hpp
+++ include/pfm_api.hpp
@@ -8,17 +8,12 @@ extern "C" __stdcall uint32_t GetEnvironmentVariableA(
 );
 
 namespace pfm {
-    namespace detail {
-        static const char* const PFM_API_ADDR_ENV_VAR = "PFM_ADJUST_PARAM_PTR_ADDRESS";
-
-        using adjust_param_ptr_t = void* (*)(void*);
-        static std::atomic<adjust_param_ptr_t> cached_adjust_param_ptr = nullptr;
-    }
-
     /// Given a pointer to some data inside a param file, will return an adjusted address 
     /// to read the "true" file without triggering an access violation.
     static void* adjust_param_ptr(void* param_data_ptr) {
-        using namespace detail;
+        using adjust_param_ptr_t = void* (*)(void*);
+        static std::atomic<adjust_param_ptr_t> cached_adjust_param_ptr = nullptr;
+        static const char* const PFM_API_ADDR_ENV_VAR = "PFM_ADJUST_PARAM_PTR_ADDRESS";
 
         if (auto func = cached_adjust_param_ptr.load(std::memory_order_acquire)) {
             return func(param_data_ptr);
